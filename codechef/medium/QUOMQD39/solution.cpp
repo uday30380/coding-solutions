edae7b2acfc4da9a496f2d38478c26f652044aaf
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-09 00:38:57 +0530

    ✅ QUOMQD39 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/QUOMQD39/solution.cpp
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Define and initialize a string variable
+        String sentence = "The sky is blue."; 
+        
+        // Using the contains() method to check if the word "sky" is present in the sentence
+        
+        
+        // The result (true/false) is directly printed to the console
+        System.out.println("Contains 'sky': " + sentence.contains("sky"));
+    }
+}
