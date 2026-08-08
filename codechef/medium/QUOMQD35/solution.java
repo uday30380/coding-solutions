223193d8379bfd8baea0aa3a77d28236fd70b355
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-08 10:19:00 +0530

    ✅ QUOMQD35 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/QUOMQD35/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Define a filePath string
+        String filePath = "C:\\Users\\Documents\\myFile.java";
+
+        // Find the last occurrence of the dot
+        int lastIndexOfDot = filePath.lastIndexOf(".");
+
+        // Print the result
+        System.out.println("The last dot is at index: " + lastIndexOfDot);
+
+    }
+}
