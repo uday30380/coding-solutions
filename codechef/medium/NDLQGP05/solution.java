64d49379b9d5d5b46fcc19ee1bcf1c893703aad9
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-09 00:56:25 +0530

    ✅ NDLQGP05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/NDLQGP05/solution.java
@@ -0,0 +1,29 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        String s1="Hello";
+        String s2="World";
+        
+        // Converting both strings to uppercase
+        String s1uppercase=s1.toUpperCase();
+        String s2uppercase=s2.toUpperCase();
+        
+
+
+
+        // Concatenating with a space in between
+        
+        String combiningthetwostrings=s1uppercase+" "+s2uppercase;
+
+
+        // Printing the length of the final string
+        System.out.println(combiningthetwostrings.length());
+
+
+        // Printing the final string
+        
+        System.out.println(combiningthetwostrings);
+
+        
+    }
+}
