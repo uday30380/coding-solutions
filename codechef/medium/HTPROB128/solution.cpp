d01103391f0fa478d22bfbc5863852169dd02871
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-08 10:35:53 +0530

    ✅ HTPROB128 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/HTPROB128/solution.cpp
@@ -0,0 +1,18 @@
+  <p>Nature provides breathtaking views, from snowy peaks to open roads through rocky landscapes. Let's explore it.</p>
+
+  <!-- TODO: Add the first image here (this one should load immediately) -->
+
+  <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470"
+ 
+  alt="A view of mountains";
+  width="600";
+  height="400";>
+
+<img src="https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"
+loading="lazy";
+alt="A view of mountains";
+width="600";
+height="400";>
+
+  <!-- Space to simulate a long page -->
+  <br><br><br><br><br><br><br><br><br><br>
