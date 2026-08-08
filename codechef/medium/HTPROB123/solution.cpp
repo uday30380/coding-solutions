daca1070e3e8ca2ea49fa2035f95e51b7d325852
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-08 10:27:08 +0530

    ✅ HTPROB123 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/HTPROB123/solution.cpp
@@ -0,0 +1,19 @@
+         height="300">
+
+    <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470" 
+         alt="A photo of a mountain range" 
+         width="400">
+
+    <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb" 
+         alt="A photo of a forest" 
+         width="600" 
+         height="400">
+
+         width="500" 
+         alt="A photo of a bustling city" 
+    <img src="https://images.unsplash.com/photo-1467269204594-9661b134dd2b" 
+
+    <h1>My Image Showcase</h1>
+</head>
+<body>
+    <title>My Image Showcase</title>
