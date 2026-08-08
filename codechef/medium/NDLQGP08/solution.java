b1df4b12f0905bcf3a2d84f19f32456683395dac
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-09 00:56:42 +0530

    ✅ NDLQGP08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/NDLQGP08/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and initialize the book name
+        String book = "It Ends With Us";
+
+        // Convert the book name to lowercase
+        String lowerBook = book.toLowerCase();
+
+        // Print the result
+        System.out.println("Lowercase Book: " + lowerBook);
+    }
+}
