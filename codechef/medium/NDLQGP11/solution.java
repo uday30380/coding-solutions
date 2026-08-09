399d090cea7847ebe5c95f1cae9010ef8ff8f693
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-09 14:55:43 +0530

    ✅ NDLQGP11 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/NDLQGP11/solution.java
@@ -0,0 +1,18 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        // Define a variable productName and its value 
+        String productName = "Samsung GALAXY Phone";
+        
+        // Use toLowerCase() method to print the product name in lowercase for storage
+        
+        String lowerproductname=productName.toLowerCase();
+        
+        
+        
+        // Print the desired results
+        
+        System.out.println("Formatted Product Name: "+lowerproductname);
+        
+    }
+}
