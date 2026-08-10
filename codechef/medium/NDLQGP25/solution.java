ef5d8ce3a4584bfc58d1c6db878e39b379e1f633
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-10 16:33:25 +0530

    ✅ NDLQGP25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/medium/NDLQGP25/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        String description = "This shirt is available in multiple colours.";
+        
+        String uppercase = description.toUpperCase();
+        
+        System.out.println(uppercase);
+        
+
+    }
+}
