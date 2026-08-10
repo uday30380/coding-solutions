9cc5d5e116583cf7251041b04e4af8253cde48bf
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-10 22:06:12 +0530

    ✅ ZWFPSZ05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/medium/ZWFPSZ05/solution.java
@@ -0,0 +1,17 @@
+// Importing the Scanner class from the java.util package
+import java.util.Scanner;
+
+class Codechef {
+    public static void main(String[] args) {
+        // Creating a Scanner object to read input from the keyboard (System.in)
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        int sum = a+b;
+        
+        System.out.println("The sum is: "+sum);
+
+    }
+}
