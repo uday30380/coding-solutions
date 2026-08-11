bd23cc040fb9dfcfe527fa9cad76f7fbb6997cb9
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-11 08:18:52 +0530

    ✅ WNDHVZ31 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/WNDHVZ31/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Taking user input for morning and evening water intake
+        int morningWater = scanner.nextInt();
+        
+        int eveningWater = scanner.nextInt();
+        
+        // Calculating total water intake
+        int totalWater = morningWater + eveningWater;
+
+        // Checking if total intake is less than 2000 ml
+        if (totalWater < 2000) {
+            System.out.println("You need to drink more water to stay hydrated!");
+        } else {
+            System.out.println("Great job! You have met your daily water intake goal!");
+        }
+    }
+}
