692d7eda630bfe9dc39fe24c078b94cd1c6c58c1
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-13 18:55:44 +0530

    ✅ HTPROB148 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/HTPROB148/solution.cpp
@@ -0,0 +1,16 @@
+
+    <audio controls preload="none">
+        <source src="https://www.w3schools.com/html/horse.mp3" type="audio/mp3">
+</audio>
+</body>
+</html>
+        <source src="https://www.w3schools.com/html/horse.mp3" type="audio/mp3">
+        Your browser does not support the audio tag."
+    </audio>
+
+    <h2>Bandwidth-Conscious Player</h2>
+    <p>This player is designed to save bandwidth. The browser will not download the audio file until you click the play button, thanks to the `preload` attribute.</p>
+    <!-- TODO: Add bandwidth-conscious audio player here -->
+    <audio controls>
+
+        Your browser does not support the audio tag."
