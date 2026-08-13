e8cfd3d7a2bd431bb542bb0b8c479182987f5182
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-13 19:13:14 +0530

    ✅ HTPROB152 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/HTPROB152/solution.cpp
@@ -0,0 +1,13 @@
+    <h1>Embedding Map Content with iframe (src attribute)</h1>
+
+    <h2>Embedded Map of a Landmark</h2>
+    
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.991625902409!2d2.294481015674066!3d48.85837007928734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9af183c51!2sEiffel%20Tower!5e0!3m2!1sen!2sus!4v1628189873130!5m2!1sen!2sus" 
+            width="700" 
+            height="500" 
+            loading="lazy"
+            title="Embedded Map of a City Landmark">
+
+</body>
+</html>
+    </iframe>
