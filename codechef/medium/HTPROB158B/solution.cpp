d89d5ad2e5235c2849d355dce286ee3bf0c36f88
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-13 19:43:00 +0530

    ✅ HTPROB158B | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/HTPROB158B/solution.cpp
@@ -0,0 +1,14 @@
+
+  <hr>
+
+  <!-- Cast section -->
+  <h2 id="cast">Cast</h2>
+  <ul>
+    <li>Leonardo DiCaprio as Cobb</li>
+    <li>Joseph Gordon-Levitt as Arthur</li>
+    <li>Elliot Page as Ariadne</li>
+    <li>Tom Hardy as Eames</li>
+  </ul>
+
+</body>
+</html>
