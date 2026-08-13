16403c52e261be051318be3ce273797ff428bef9
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-13 19:23:14 +0530

    ✅ HTPROB156 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/HTPROB156/solution.cpp
@@ -0,0 +1,19 @@
+    <hr>
+    </iframe>
+
+        title="Rick Astley - Never Gonna Give You Up (Official Music Video)">
+        allowfullscreen
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+        frameborder="0" 
+    <iframe 
+    <p>This video is for general viewing, with all the standard controls available to the user.</p>
+</head>
+<body>
+
+    <h1>Two Video Embeds</h1>
+    <p>This page demonstrates how to embed and customize two separate YouTube videos for different purposes on a single page.</p>
+
+    <h2>Standard Video Player</h2>
+    <title>Two Video Embeds</title>
