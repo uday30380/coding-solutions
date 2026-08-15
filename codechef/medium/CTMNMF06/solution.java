39266e265280cdb42eb1939dfc5c101570028027
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-15 11:27:20 +0530

    ✅ CTMNMF06 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

---
+++ codechef/medium/CTMNMF06/solution.java
@@ -0,0 +1,15 @@
+class Codechef {
+    public static void main(String[] args) {
+
+        String correctPassword = "JavaRocks";
+        String enteredPassword = "JavaRocks";
+
+        // Write the condition below in the while loop
+        while (correctPassword!=enteredPassword) {
+            System.out.println("Invalid Password");
+            
+        }
+        System.out.println("Access Granted!");
+
+    }
+}
