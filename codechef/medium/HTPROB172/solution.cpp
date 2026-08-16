a13c86cbf100a858eb54f9b58d8916f4acb8d725
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-16 17:28:36 +0530

    ✅ HTPROB172 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/HTPROB172/solution.cpp
@@ -0,0 +1,18 @@
+
+    </header>
+        </nav>
+    <h2>Featured Products</h2>
+    <ul>
+        <li><a href="#">E-Reader Pro</a></li>
+        <li><a href="#">Smart Watch 5</a></li>
+        <li><a href="#">Noise-Canceling Headphones</a></li>
+    </ul>
+
+    <!-- TODO: Wrap the following content inside main tags-->
+     <main>
+
+        <h1>The Quantum Laptop</h1>
+        <p>
+            Experience unparalleled performance with the new Quantum Laptop. Its sleek design and powerful
+            processor make it the ideal choice for both creative professionals and casual users.
+        </p>
