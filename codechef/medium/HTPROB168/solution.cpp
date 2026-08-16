e6a0c90dbb155473c5b94845417c00d9a6aa7633
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-16 17:09:09 +0530

    ✅ HTPROB168 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/HTPROB168/solution.cpp
@@ -0,0 +1,18 @@
+    
+    </header>
+      <a href="#">Contact</a>
+      <a href="#">About</a> |
+      <a href="#home">Home</a> |
+    
+    <header>
+    <h1>My Blog</h1>
+   <!-- TODO: Add main site header and nav  -->
+
+<body>
+</head>
+      <nav>
+
+
+  <!-- TODO: Add article header  -->
+    </nav>
+  <title>Blog Post Structure</title>
