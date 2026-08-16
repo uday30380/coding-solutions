81856452b96d6986758bf1cdb0970dee369774f0
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-16 12:05:57 +0530

    ✅ CTMNMF34 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/CTMNMF34/solution.cpp
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+     
+       // This infinite loop simulates a continuously running server.
+        while(true)
+        {
+             System.out.println("Server is running");
+             break;
+        }
+    
+    }
+}
