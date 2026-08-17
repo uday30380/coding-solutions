42d9cb853be1464ee6908bd1398e6f5f579f9ac6
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-17 20:57:32 +0530

    ✅ CATMS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/CATMS/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner= new Scanner(System.in);
+		
+		int n=scanner.nextInt();
+		
+		
+
+	}
+}
