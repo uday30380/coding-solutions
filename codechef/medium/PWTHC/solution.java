5e201cc55dbc9d65f11910994c6fe5f50723f339
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-17 20:53:19 +0530

    ✅ PWTHC | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/PWTHC/solution.java
+++ codechef/medium/PWTHC/solution.java
@@ -7,14 +7,6 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner scanner = new Scanner(System.in);
-		
-		int first=scanner.nextInt();
-		int middle = scanner.nextInt();
-		int last = scanner.nextInt();
-		
-		
-		int sum=
 
 	}
 }
