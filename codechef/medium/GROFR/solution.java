7bb361961c125b734eb8a28262224beb0c81cf44
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-17 20:58:49 +0530

    ✅ GROFR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/GROFR/solution.java
+++ codechef/medium/GROFR/solution.java
@@ -6,7 +6,10 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner scanner = new Scanner(System.in);
+		
+		n=scanner.nextInt();
+		String 
 
 	}
 }
