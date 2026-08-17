28670185c6532122939a972fd3ad406c5376d279
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-17 17:13:46 +0530

    ✅ ZCUWOB03 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/ZCUWOB03/solution.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Loop to print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("I love coding " + i); // Print statement with iteration value
+        }
+    }
+}
