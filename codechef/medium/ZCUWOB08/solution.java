9ec811ef3e0fa3d0202178ac9a4848a86ccbc7b4
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-17 19:26:46 +0530

    ✅ ZCUWOB08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/ZCUWOB08/solution.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main(String[] args) {
+        int start = 5; // Define the starting number
+        int end = 9; // Define the ending number
+
+        // Loop to print numbers from start to end
+        for (int i = start; i <= end; i++) {
+            System.out.println(i + i); // Print the current number
+        }
+    }
+}
