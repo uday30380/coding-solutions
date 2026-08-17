8eefc4559f951032ccd1ca56df2bacf691f5cc4d
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-17 21:15:29 +0530

    ✅ CATMS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/CATMS/solution.java
+++ codechef/medium/CATMS/solution.java
@@ -1,16 +1,25 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner scanner= new Scanner(System.in);
-		
-		int n=scanner.nextInt();
-		
-		
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int N = sc.nextInt();
+
+        int count = 0;
+        int minSpeed = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int speed = sc.nextInt();
+
+            if (speed < minSpeed) {
+                count++;
+                minSpeed = speed;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
 }
