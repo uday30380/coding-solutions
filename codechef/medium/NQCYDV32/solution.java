e3b172699092df9f66ed2eeff98aab3254045e6e
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-19 23:48:17 +0530

    ✅ NQCYDV32 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/NQCYDV32/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class CodeChef {
+    public static void main(String[] args) {
+        int numerator = 10;
+        int denominator = 1; // Change the denominator to 1 to avoid the error an1d get correct output.
+       
+        int result = numerator / denominator;
+        System.out.println("Age is " + result);
+}
+}
