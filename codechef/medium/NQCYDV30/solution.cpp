a3062fd2be6f4e03dbf13f282c1a23ef6fe5fc96
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-19 23:46:57 +0530

    ✅ NQCYDV30 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/NQCYDV30/solution.cpp
@@ -0,0 +1,10 @@
+class CodeChef {
+    public static void main(String[] args) {
+        int age = 28;
+        if (age % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
