3c28837ec10532c9be6ebaadd9ee24a5a398923c
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-19 23:51:05 +0530

    ✅ NQCYDV38 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/NQCYDV38/solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class CodeChef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println("" + s.charAt(0) + s.charAt(3) + s.charAt(0));
+    }
+}
