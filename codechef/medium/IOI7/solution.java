8fb91a85472161d932de352955ebe887f58bd5e0
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-19 20:21:43 +0530

    ✅ IOI7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/IOI7/solution.java
+++ codechef/medium/IOI7/solution.java
@@ -6,7 +6,17 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		Scanner scanner = new Scanner(System.in);
 		
+		int n=scanner.nextInt();
+		int x=scanner.nextInt();
+		
+		
+		if(n>x){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
 
 	}
 }
