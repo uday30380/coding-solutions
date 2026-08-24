bf37cb6565fd8e74787c3907a3bf8e77b499aa48
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-24 20:05:02 +0530

    ✅ WITRH | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/WITRH/solution.java
+++ codechef/medium/WITRH/solution.java
@@ -6,7 +6,14 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-
+	Scanner sc = new Scanner(System.in);
+	
+	int x=sc.nextInt();
+	int y=sc.nextInt();
+	int k=sc.nextInt();
+	
+	
+	
+	
 	}
 }
