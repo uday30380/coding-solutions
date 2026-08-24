c1d00bc4f0dc9d664239dbd2bc1eef60264f561b
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-24 22:03:08 +0530

    ✅ JAVATC04 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/JAVATC04/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		try{
+		    int result=n;
+		    System.out.println(result);
+		}catch(ArithmeticException e){
+		    System.out.println("Invalid Integer");
+		}
+
+	}
+}
