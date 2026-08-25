4ce5876d9e241418a63afbef5cc548b60c1b849a
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-25 19:45:40 +0530

    ✅ ARRLST14B | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/ARRLST14B/solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+class Codechef {
+    public static void main(String[] args) {
+        // Step 1: Declare and initialize the ArrayList
+        ArrayList<Integer> marks = new ArrayList<>();
+        marks.add(48);
+        marks.add(70);
+        marks.add(86);
+
+        // Step 2: Update each element manually (split get and set)
+        
+        marks.set(0,50);
+        marks.set(1,72);
+        marks.set(2,88);
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        // Step 3: Print updated elements manually
+        System.out.println("Updated Marks:");
+        System.out.println(marks.get(0));
+        System.out.println(marks.get(1));
+        System.out.println(marks.get(2));
+    }
+}
