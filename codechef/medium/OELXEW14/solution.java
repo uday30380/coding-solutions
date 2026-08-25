cfdfee0a693c444f25dd334c58e9104192b9c812
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-25 16:55:58 +0530

    ✅ OELXEW14 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/OELXEW14/solution.java
@@ -0,0 +1,21 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Step 1: Declare and initialize the array
+        int[] marks = {48, 70, 86};
+
+        // Step 2: Update each element manually (no loop)
+        
+        marks[0]=50;
+        marks[1]=72;
+        marks[2]=88;
+
+
+
+
+        // Step 3: Print updated elements manually
+        System.out.println("Updated Marks:");
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
