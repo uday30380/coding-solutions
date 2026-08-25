d318bfff1221bfd0809147776558b9df0f9b39a0
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-25 19:35:30 +0530

    ✅ ARRLST10B | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/ARRLST10B/solution.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class Codechef {
+
+    public static void main(String[] args) {
+        
+        int Week1=28, Week2=30, Week3=27;
+        ArrayList<Integer> attendanceList = new ArrayList<>();
+
+        // Step 2: Add attendance for 3 weeks
+        attendanceList.add(Week1);
+        attendanceList.add(Week2);
+        attendanceList.add(Week3);
+
+
+
+
+        // Step 3: Access and print attendance using .get() method
+        
+        System.out.println("Attendance in Week 1:"+Week1);
+        System.out.println("Attendance in Week 3:"+Week3);
+        
+        
+        
+
+
+    }
+}
