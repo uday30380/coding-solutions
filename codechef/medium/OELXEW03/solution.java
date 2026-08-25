c19e1452ec793fb2f3e806b062d52f607a91374c
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-25 15:14:34 +0530

    ✅ OELXEW03 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/OELXEW03/solution.java
@@ -0,0 +1,9 @@
+// This is a class named Codechef
+class Codechef {
+    public static void main(String[] args) {
+
+       
+
+        System.out.println("Array for city names declared but not yet initialized.");
+    }
+}
