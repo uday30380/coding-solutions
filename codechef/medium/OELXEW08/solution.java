418ad75cf3d96cd2f296cfd5d442afd77ecd93ac
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-25 15:16:09 +0530

    ✅ OELXEW08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/OELXEW08/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+
+        // Here we are storing String type(will be enclosed in " ") of values in array 
+        String[] employeeNames = {"Alex","Bobby"}; // Complete the code here
+
+        // Printing the array, will be discussed in next knowledge point
+        System.out.println("Employee 1: " + employeeNames[0]);
+        System.out.println("Employee 2: " + employeeNames[1]);
+
+    }
+}
