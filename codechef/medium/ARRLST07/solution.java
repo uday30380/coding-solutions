81f8313e6fb2fc0d2ed3ba7dc5a72b9bce887e38
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-25 19:21:26 +0530

    ✅ ARRLST07 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/ARRLST07/solution.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Codechef {
+    public static void main(String[] args) {
+
+        // Here we are storing String type (will be enclosed in " ") of values in ArrayList
+        ArrayList<String> employeeNames = new ArrayList<>();
+        
+        // Complete the code below
+        
+        employeeNames.add("Alex"); // Fill here
+        employeeNames.add("Bobby"); // Fill here
+
+        // Printing the ArrayList elements, will be discussed in next knowledge point
+        System.out.println("Employee 1: " + employeeNames.get(0));
+        System.out.println("Employee 2: " + employeeNames.get(1));
+
+    }
+}
