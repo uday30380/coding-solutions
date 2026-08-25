c2c5c7a6cb6d76f60f1acc26bc2755581b8e9986
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-25 19:10:57 +0530

    ✅ ARRLST03 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-25

---
+++ codechef/medium/ARRLST03/solution.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Codechef {
+    public static void main(String[] args) {
+        // Declare an ArrayList for storing city names (not yet initialized)
+        ArrayList<String> cities;
+
+        // Display a message
+        System.out.println("ArrayList for city names declared but not yet initialized.");
+    }
+}
