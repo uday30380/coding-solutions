8d0366bf206c66bb35ed7a8018fb5a88ed38021e
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-26 22:11:51 +0530

    ✅ REGCLN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/REGCLN/solution.java
+++ codechef/medium/REGCLN/solution.java
@@ -10,16 +10,7 @@ class Codechef
 	    Scanner scanner = new Scanner(System.in);
 	    int n=scanner.nextInt();
 	    
-	    if(n>=20&&n<=29){
-	        System.out.println(30-n);
-	    }
-	    else if(n>=30&&n<=39){
-	        System.out.println(40-n);
-	    }
-	    else
-	    {
-	        System.out.println(50-n);
-	    }
+	    if()
 
 	}
 }
