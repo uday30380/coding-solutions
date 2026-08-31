f208f1e2a6ffe49943574be8da1b07477de8ea44
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-08-31 20:12:21 +0530

    ✅ SCOCN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.java
+++ codechef/medium/SCOCN/solution.java
@@ -1,12 +1,33 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        // 1. Get the desired size of the array
+       
+        int size = scanner.nextInt();
+        
+        int fianl = scanner.nextInt();
+
+        // 2. Initialize the array with that size
+        int[] userArray = new int[size];
+
+        
+        for (int i = 0; i < size; i++) {
+            userArray[i] = scanner.nextInt();
+        }
+        
+        for(int i=0;i<size;i++){
+        userArray[i]=fianl;
+        i++;
+        System.out.println(i);
+        
+        
+            
+            
+        }
+        
+        
+    }
 }
