ed50af69faaef542744e896658f0f83a707d682a
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-09-02 07:33:10 +0530

    ✅ MONKCI05 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/MONKCI05/solution.cpp
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class Codechef {
+    public static void main(String[] args) {
+        ArrayList<String> subjects = new ArrayList<>();
+
+        // Add "Math" at the end
+        
+        subjects.add("Art");
+
+
+        // Add "English" at the end
+        
+        subjects.add("Math");
+
+
+        // Add "Science" at index 1
+        
+        subjects.add("Science");
+
+
+        // Add "History" at index 2
+        subjects.add("History");
+
+
+        // Add "Art" at index 0
+        
+        subjects.add("English");
+
+
+        // Print the final list
+        System.out.println(subjects);
+    }
+}
