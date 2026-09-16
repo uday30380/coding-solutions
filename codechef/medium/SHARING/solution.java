00db658ef5a17dcef91dc043bdab540566c85e64
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-09-16 21:30:00 +0530

    ✅ SHARING | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/SHARING/solution.java
+++ codechef/medium/SHARING/solution.java
@@ -1,21 +1,20 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int alice = sc.nextInt();
-		int bob = sc.nextInt();
-		
-		int alice = bob+;
-		
-		int final2 =2+bob;
-		
-		
+public class Main {
+    public static void main(String[] args) {
 
-	}
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int total=A + B;
+
+        if (total% 2 == 0) {
+            int give = (A - B) / 2;
+            System.out.println(give);
+        } else {
+            System.out.println(-1);
+        }
+    }
 }
