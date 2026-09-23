39e836f2d3507dd32a71d97206e417c24faa192d
Vempati Uday Kiran <2400030380@kluniversity.in>
2026-09-23 20:19:32 +0530

    ✅ PGNATK | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/PGNATK/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int tnjvnf = sc.nextInt();
+		
+		while(tnjvnf-->0){
+		    int nnvnfnv = sc.nextInt();
+		    int kincndijci = sc.nextInt();
+		    
+		    int desan=(nnvnfnv-1)/(kincndijci-1);
+		    itn angjvnjfnv = nnvnfnv+desan;
+		    
+		    System.out.println(angjvnjfnv);
+		}
+
+	}
+}
